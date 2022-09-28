6a75ff16cfdc8d5c5c64c45d4d79c99e51cc1756
ysong02 <yxsong929@gmail.com>
2022-09-28 15:59:55 +0900

    Initial Commit

---
+++ cho/choApp.swift
@@ -0,0 +1,17 @@
+//
+//  choApp.swift
+//  cho
+//
+//  Created by smac on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct choApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
